00712bf4480f2bbc3ab24e2d75cb060f91562dd0
Ian Deans <ideans715@gmail.com>
2017-01-18 10:57:07 -0800

    Clean up old files | add style entries

--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import * as Components from './components/components'
+import * as Model from './components/models'
+// import Game from './components/Game'
+import { createStore } from 'redux'
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// import App from './App';
+
+const reducer = ( state = new Model.Game(), action ) => {
+  switch (action.type) {
+    case 'TICK':
+    console.log('TICK')
+      return state.tick()
+    default:
+      return state
+  }
+  //return state
+
+}
+
+let store = createStore( reducer )
+
+store.subscribe( () => {
+  ReactDOM.render(
+    <Components.GameView game={ store.getState() } />,
+    document.getElementById( 'root' )
+  )
+})
+
+setInterval( () => store.dispatch({ type: 'TICK' }), 500 )
+
+
+
+// ReactDOM.render(
+//   <div>
+//     { data.map( c => <Components.ShapeView shape={c} /> )}
+//   </div>,
+//   document.getElementById('root')
+// )
+
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+
+{/*  */}
