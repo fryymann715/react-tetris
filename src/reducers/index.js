fde8c449d5ffea8ab7d7661320fc268f88a8de06
Ian Deans <ideans715@gmail.com>
2017-05-02 16:26:26 -0700

    Remove everything but build folder

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import gameOneReducer from './game_one'
-import gameTwoReducer from './game_two'
-import { combineReducers } from 'redux'
-
-const gameReducer = () => {
-
-}
-
-export rootReducer = combineReducers( gameReducer, gameOneReducer, gameTwoReducer )
